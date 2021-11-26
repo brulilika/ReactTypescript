3491528e4d09b2cf8cfa3a084874cf203d878a4c
Bruna Lika <brunalika@msn.com>
2021-11-26 11:00:28 -0200

    Navbar itens

--- dashboard-react/src/components/navigationBar/index.tsx
+++ dashboard-react/src/components/navigationBar/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg'
 
-
-import { Container }  from './styles';
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp,
+} from 'react-icons/md'
+import { Container, Header, LogImg, Title, MenuContainer,MenuItemLink }  from './styles';
 
 const NavigationBar: React.FC = () => {
 
     return (
         <Container >
-            <p>NavBar</p>
+            <Header>
+                <LogImg src={logoImg} alt="Logo imagem"/>
+                <Title>Carteira Simulator</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#"> 
+                    <MdDashboard/> Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#"> 
+                    <MdArrowDownward/> Entrada
+                </MenuItemLink>
+                <MenuItemLink href="#"> 
+                    <MdArrowUpward/>Saida
+                </MenuItemLink>
+                <MenuItemLink href="#"> 
+                    <MdExitToApp/>Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     );
 }
--- dashboard-react/src/components/navigationBar/styles.ts
+++ dashboard-react/src/components/navigationBar/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 
-
 export const Container = styled.div`
     grid-area: AS;
     
@@ -9,4 +8,42 @@ export const Container = styled.div`
     padding-left: 20px;
     border-right: 1px solid #FFF;
     position: relative;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    height: 70px;
+`;
+
+export const Title = styled.h3`
+    color:#FFF;
+    margin-left: 5px;
+`;
+
+export const LogImg = styled.img`
+    height: 40px;
+    width: 40px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top:30px;
+`;
+
+export const MenuItemLink = styled.a`
+    margin-top: 10px;
+    color: #EED7CE;
+    text-decoration: none;
+    transition: opacity 0.3s;
+    display: flex;
+    align-items: center;
+    &:hover{
+        opacity: 0.6;
+    }
+    >svg{
+        font-size: 20px;
+        margin-right: 5px;
+    }
 `;
